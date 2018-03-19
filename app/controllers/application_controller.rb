218fca07bff7d55aeb68835421044c29d38ec453
Sara Tibbetts <sxt4866@rit.edu>
2018-03-19 15:25:41 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,8 @@ class ApplicationController < Sinatra::Base
   
   get '/recipes' do 
     #### 
-    @recipe1 = 
+    @@recipe1 = Recipe.create(:name => recipe_name, :ingredients => recipe_ingredients, :cook_time => recipe_cook_time)
+    @recipe2 = Recipe.create(:name => "waldorf salad", :ingredients => "apples, cabbage, oil, vinegar", :cook_time => "0")
     ### 
     
     @recipes = Recipe.all
